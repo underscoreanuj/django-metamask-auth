35ccadc56e043ecd06d20ded4ea18d5b7616b564
underscoreanuj <underscoreanuj@gmail.com>
2020-07-21 12:34:18 +0530

    fixed untracked files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/Anuj Singh/Desktop/Anuj/eth_india_fellowship/django-web3-auth/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
